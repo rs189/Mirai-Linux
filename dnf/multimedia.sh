22618349fa6163ea173f52f3fbfef68f74215bf1
rs189 <35667100+rs189@users.noreply.github.com>
2025-08-01 13:28:13 +0900

    fix: multimedia codecs prompt

--- dnf/multimedia.sh
+++ dnf/multimedia.sh
@@ -6,7 +6,7 @@ sudo dnf install -y libheif-freeworld libheif-tools ffmpeg mpv vlc ffmpegthumbna
 sudo dnf install -y gstreamer1 gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free gstreamer1-plugins-ugly gstreamer1-libav
 
 sudo dnf4 config-manager --enable fedora-cisco-openh264 -y || true
-sudo dnf install openh264 mozilla-openh264 libavcodec-freeworld gstreamer1-plugins-bad-freeworld
+sudo dnf install -y openh264 mozilla-openh264 libavcodec-freeworld gstreamer1-plugins-bad-freeworld
 
 sudo akmods --force
 sudo modprobe v4l2loopback
